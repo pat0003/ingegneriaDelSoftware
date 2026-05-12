b8b756b866ffe7b9a67b6fe5fa10c8ce14c2a4e6
Sich <antoninisimone0228@gmail.com>
2026-05-12 19:44:44 +0200

    ristrutturazione package

---
+++ src/main/java/it/unicam/hackhub/HackHubApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.hackhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackHubApplication.class, args);
+        System.out.println("HackHub Spring Boot avviato con successo! 🚀");
+    }
+}
